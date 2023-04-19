3f2b9b769b0e71adefcda6e5c780fd72a16e3f41
JAKUBBUJAK123 <jakbuj3@gmail.com>
2023-04-19 12:59:52 +0200

    2023-04-19 JB

---
+++ analyser.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep='\n')
+
+product_code = input("Podaj kod produktu: ")
+
+opinions  = pd.read_json(f"./opinions/{product_code}.json")
+print(opinions)
+
+
+opinion_count = len(opinions.index)
+pros_count= sum([False if len(p)==0 else True for p in opinions.pros])
+cons_count = sum([False if len(c)==0 else True for c in opinions.cons])
+pros_count = opinions.pros.map(lambda p: False if len(p)==0 else True).sum()
+cons_count =  opinions.cons.map(lambda c: False if len(c)==0 else True).sum()
+average_score = 0
+print(pros_count)
--- scraper.py
+++ scraper.py
@@ -1,73 +1,53 @@
 import requests
-from bs4 import BeautifulSoup
 import json
+from bs4 import BeautifulSoup
 
-def get_cos(ancestor, selector = None , attribute =  None , return_list = False) :
+def get_element(ancestor, selector = None, attribute = None, return_list = False):
     try:
         if return_list:
-             [tag.text.strip() for tag in ancestor.select(selector)].copy()
+            return [tag.text.strip() for tag in ancestor.select(selector)].copy()
         if not selector and attribute:
             return ancestor[attribute]
         if attribute:
             return ancestor.select_one(selector)[attribute].strip()
         return ancestor.select_one(selector).text.strip()
-    except AttributeError:
+    except (AttributeError, TypeError):
         return None
 
-
 selectors = {
-        "opinion_id": [ None , "data-entry-id"],
-        "author": ["span.user-post__author-name"],
-        "recommentadion": [ "span.user-post__author-recomendation"],
-        "score": ["span.user-post__score-count"],
-        "purhased": ["div.review-pz"],
-        "published_at": ["span.user-post__published > time:nth-child(1)" , ],
-        "purhased_at": ["span.user-post__published > time:nth-child(2)" , ] ,
-        "thumbs_up": ["button.vote-yes > span"],
-        "thumbs_down": ["button.vote-no > span"],
-        'content': ['div.user-post__text'],
-        'pros' : ["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item" ,None , True],
-        'cons' : ["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item" , None, True],
-    }
+    "opinion_id": [None, "data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "score": ["span.user-post__score-count"],
+    "purchased": ["div.review-pz"],
+    "published_at": ["span.user-post__published > time:nth-child(1)","datetime"],
+    "purchased_at": ["span.user-post__published > time:nth-child(2)","datetime"],
+    "thumbs_up": ["button.vote-yes > span"],
+    "thumbs_down": ["button.vote-no > span"],
+    "content": ["div.user-post__text"],
+    "pros": ["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item",None, True],
+    "cons": ["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item",None, True]
+}
 
-
-# product_code = input("Podaj kod produtu")
-product_code = "96685108"
-page_no = 1
-al_opinions = []
+product_code = input("Podaj kod produktu: ")
+#product_code = "96685108"
+all_opinions = []
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 while(url):
-    
     print(url)
-    response = requests.get(url, allow_redirects=False)
-    
-    
-    page = BeautifulSoup(response.text, "html.parser")
+    response = requests.get(url)
+    page = BeautifulSoup(response.text, 'html.parser')
     opinions = page.select("div.js_product-review")
-
-
-#opinion_id = ["data-entry-id"]
-#author = "span.user-post__author-name"
-#recommentadion = "span.user-post__source-count"
-#score = "span.user-post__score-count"
-#purhased = "div.reviev-pz"
-#published_at = 'span.user-post__published > time:nth-child(1)["datetime"]'
-#purhased_at = 'span.user-post__published > time:nth-child(2)["datetime]'
-#thumbs_up = "span[id^=votes-yes]"
-#content = "div.user-post__text"
-
-    
     for opinion in opinions:
-        single_option = {}
+        single_opinion = {}
         for key, value in selectors.items():
-            single_option[key] = get_cos(opinion, *value)
+            single_opinion[key] = get_element(opinion,*value)
+        all_opinions.append(single_opinion)
+    try:
+        url = "https://www.ceneo.pl"+get_element(page, "a.pagination__next", "href")
+    except TypeError:
+        url = None
 
-        al_opinions.append(single_option)
-    
-    url = f"https://www.ceneo.pl" + get_cos(page, 'a.pagination__next' , 'href')
-print(len(al_opinions))
-
-
-with open(f"./opinions/{product_code}.json" , 'w' , encoding='utf-8') as jf:
-    json.dump(al_opinions, jf, indent=4 , ensure_ascii=False)
-print(json.dumps(al_opinions , indent=4 , ensure_ascii=False))
+print(len(all_opinions))
+with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent=4,ensure_ascii=False)
